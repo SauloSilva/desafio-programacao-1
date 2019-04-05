39833e5f0ce8f8037bb2bb8fcbf11f4118457d14
Saulo Santiago <saulodasilvasantiago@gmail.com>
2019-04-05 18:26:29 -0300

    Organize coverage in groups

--- spec/support/simplecov.rb
+++ spec/support/simplecov.rb
@@ -1,3 +1,9 @@
 require 'simplecov'
 SimpleCov.minimum_coverage 100
-SimpleCov.start
+SimpleCov.start 'rails' do
+  minimum_coverage 100
+
+  add_group 'Services', 'app/services'
+  add_group 'Builders', 'app/builders'
+  add_group 'Uploaders', 'app/uploaders'
+end
